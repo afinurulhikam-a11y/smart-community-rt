7836c7c02bb339b6759053bbf73da0c269701bde
affie23 <junior.pa39ong@gmail.com>
2026-08-05 03:37:56 +0700

    fix: correct relative import paths in siskamling_screen.dart for web compilation

--- frontend-flutter/lib/screens/admin/siskamling_screen.dart
+++ frontend-flutter/lib/screens/admin/siskamling_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/intl.dart';
-import '../../../core/responsif.dart';
-import '../../../providers/patrol_provider.dart';
-import '../../../providers/permission_provider.dart';
-import '../../../widgets/tabel_responsif.dart';
+import '../../core/responsif.dart';
+import '../../providers/patrol_provider.dart';
+import '../../providers/permission_provider.dart';
+import '../../widgets/tabel_responsif.dart';
 import '../../core/theme/app_theme.dart';
 import '../../core/theme/warna_konteks.dart';
 
