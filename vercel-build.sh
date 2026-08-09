9a795b2b6f3d530a959fa6faafe84961dfbf2b2b
affie23 <junior.pa39ong@gmail.com>
2026-08-09 14:24:38 +0700

    refactor: ekstrak vercel buildCommand ke berkas vercel-build.sh

---
+++ vercel-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable --depth 1
+fi
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter config --no-analytics --enable-web
+cd frontend-flutter
+flutter pub get
+flutter build web --release
